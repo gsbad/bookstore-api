4b2c25c40befb3c6a1074580be6a97fa225af8ee
gsbad <gsbadbr@gmail.com>
2024-03-04 20:03:30 -0300

    POST para Livro

--- src/main/java/com/sefirot/bookstore/service/LivroService.java
+++ src/main/java/com/sefirot/bookstore/service/LivroService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import com.sefirot.bookstore.domain.Categoria;
 import com.sefirot.bookstore.domain.Livro;
 import com.sefirot.bookstore.repositories.LivroRepository;
 import com.sefirot.bookstore.service.exceptions.ObjectNotFoundException;
@@ -41,5 +41,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
